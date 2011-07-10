03a4b25e8430749d825285e61a7ffb5c0c735802
Yinhai Liu <meteor.yinhai@gmail.com>
2011-07-11 04:49:09 +0800

    no change

--- src/preprocessing/CreateBWImageNoJAI.py
+++ src/preprocessing/CreateBWImageNoJAI.py
@@ -5,12 +5,11 @@
  * Rafael Santos (rafael.santos@lac.inpe.br)
  */'''
 
-from java.awt.image.BufferedImage import *;
-import java.awt.image.WritableRaster;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage as BufferedImage;
+import java.awt.image.WritableRaster as WritableRaster;
+import java.io.File as File;
+import java.io.IOException as IOException;
+import javax.imageio.ImageIO as ImageIO;
 
 '''
 /**
@@ -19,26 +18,25 @@ import javax.imageio.ImageIO;
  * The JAI API is not used in this example.
  */'''
 
-#class CreateBWImageNoJAI:
-#def  main(self, args):
+class CreateBWImageNoJAI:
+    def  main(self, args):
+        width = 400;
+        height = 400;
+        im =  BufferedImage(width,height,BufferedImage.TYPE_BYTE_BINARY);
+        raster = im.getRaster();
+        for h in range(height):
+            for w in range(width):
+                if (h / 50 + w / 50) % 2 == 0:
+                    raster.setSample(w,h,0,0);
+                else:
+                    raster.setSample(w,h,0,1);
+        try:
+            ImageIO.write(im,"PNG", File("checkboard.png"));
+        except IOException:
+            print "can not open the file"
 
-width = 400;
-height = 400;
-im =  BufferedImage(width,height,BufferedImage.TYPE_BYTE_BINARY);
-raster = im.getRaster();
-for h in range(height):
-    for w in range(width):
-        if (h / 50 + w / 50) % 2 == 0:
-            raster.setSample(w,h,0,0);
-        else:
-            raster.setSample(w,h,0,1);
-try:
-    ImageIO.write(im,"PNG", File("checkboard.png"));
-except IOException:
-    print "can not open the file"
 
-
-#if __name__ == '__main__':
-    # c = CreateBWImageNoJAI()
-    # c.main(None)
+if __name__ == '__main__':
+    c = CreateBWImageNoJAI()
+    c.main(None)
 
